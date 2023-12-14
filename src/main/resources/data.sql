93858160c4f68905cef6b1543e33fbd600d9aac1
Mathildergnr <mthldregnier@gmail.com>
2023-12-14 22:51:28 +0200

    commit data.sql correct errors

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 --supprimer les lignes de table
 DELETE FROM student;
 --ajouter des données dans la table
-INSERT INTO student(id, name, age) VALUES (1, 'Mathilde', 23);
-INSERT INTO student(id, name, age) VALUES (2, 'Guillaume', 21);
-INSERT INTO student(id, name, age) VALUES (3, 'Diane', 18);
-INSERT INTO student(id, name, age) VALUES (4, 'Vincent', 25);
-INSERT INTO student(id, name, age) VALUES (5, 'Michel', 85);
-INSERT INTO student(id, name, age) VALUES (6, 'Léa', 8);
+--INSERT INTO student(id, name, age) VALUES (1, 'Mathilde', 23);
+--INSERT INTO student(id, name, age) VALUES (2, 'Guillaume', 21);
+--INSERT INTO student(id, name, age) VALUES (3, 'Diane', 18);
+--INSERT INTO student(id, name, age) VALUES (4, 'Vincent', 25);
+--INSERT INTO student(id, name, age) VALUES (5, 'Michel', 85);
+--INSERT INTO student(id, name, age) VALUES (6, 'Léa', 8);
 
 -- Laissez la base de données générer automatiquement l'ID
 --INSERT INTO student(name, age) VALUES ('Michel', 85);
@@ -21,7 +21,7 @@ INSERT INTO student(id, name, age) VALUES (6, 'Léa', 8);
 --supprimer l'étudiant où l'id est 5
 --DELETE FROM student WHERE id = 5;
 
-CREATE SEQUENCE seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS seq START WITH 1 INCREMENT BY 1;
 
 --récupérer toutes les données de la table
 SELECT * FROM student;
